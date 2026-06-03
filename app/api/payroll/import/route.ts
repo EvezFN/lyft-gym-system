7418d2d48d494f4760294440fe0406fb4397039a
EvezFN <79392276+EvezFN@users.noreply.github.com>
2026-06-03 00:25:50 -0400

    Update route.ts

--- app/api/payroll/import/route.ts
+++ app/api/payroll/import/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import * as XLSX from 'xlsx';
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from '../../../../utils/supabase/server';
 
 export async function POST(req: NextRequest) {
   try {
